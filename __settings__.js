8b0a5a4adb224b8de028466ec01d1f7a85ef5125
Michael Collins <michael-collins@users.noreply.github.com>
2024-03-29 21:45:05 -0400

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1940812.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'preferWebGpu': false,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 169217036, 169405615, 169438149, 169438151, 169438546, 170573930, 171101242 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/169184481/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/169184482/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/169184480/1/ammo.js', 'preload' : true},
+];
